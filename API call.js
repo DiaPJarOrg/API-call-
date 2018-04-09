d65c81af0f79e385ebd6619a58816ebefab75e1f
DiaPJar <37661118+DiaPJar@users.noreply.github.com>
2018-04-09 16:13:40 +0100

    Update API call.js

--- API call.js
+++ API call.js
@@ -1,4 +1,5 @@
-function ApiCall() {
+/*
+    
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", "https://ghibliapi.herokuapp.com/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49", false);
     xhttp.setRequestHeader("Content-type", "application/json");
@@ -10,17 +11,51 @@ function ApiCall() {
   .then(response => response.json())
   .then(json => console.log(json))
   */
+
+
+function DysplayMovieInConsole(){
+var x= filmApiCall("58611129-2dbc-4a81-a72f-77ddfc1b1b49");
+console.log(x);
+}
+
+function filmApiCall(id) {
+var movie = GETAPICall("https://ghibliapi.herokuapp.com/films/" + id);
+return movie;
 }
 
 
+function GETAPICall(url) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", url, false);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send();
+    var response = JSON.parse(xhttp.responseText);
+	return response;
+};
+    
+    
+ /*
 function ApiCall1() {
     var xhttp1 = new XMLHttpRequest();
     xhttp1.open("POST", "https://reqres.in/api/register ", false);
     xhttp1.setRequestHeader("Content-type", "application/json");
-    xhttp1.send(JSON.stringify({"email": "sydney@fife",
-    "password": "pistol"})
-    
-);
+    xhttp1.send(JSON.stringify({"email": "sydney@fife", "password": "pistol"}));
     console.log(xhttp1.status);  
 }
+*/   
+    
+    
+    
+function POSTAPICall(url, body) {
+    var xhttp1 = new XMLHttpRequest();
+    xhttp1.open("POST", url, false);
+    xhttp1.setRequestHeader("Content-type", "application/json");
+    xhttp1.send(body);
+    return xhttp1;
+    
+}
 
+function determinatePOSTUrlAndBody() {
+var newUser = POSTAPICall("https://reqres.in/api/register",'{"email": "sydney@fife", "password": "pistol"}');
+console.log(newUser.status);
+}
